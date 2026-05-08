6ea8199429243498a7ad25e9c4e46f30fbef9905
renizex <xr4tygd@gmail.com>
2026-05-08 23:53:41 +0300

    init actions.py

---
+++ actions.py
@@ -0,0 +1,44 @@
+def name_create(ui):
+    while True:
+        ui.display("\nназови же имя того, кому суждено страдать")
+        name = ui.get_input("> ").strip()
+        while name == "" or len(name) < 3:
+            ui.display("\nтакое имя создать нельзя")
+            name = ui.get_input("> ").strip()
+        while True:
+            ui.display(f"\nотлично, имя твое - {name}. нажми Enter, если хочешь продолжить и нет, если хочешь сменить имя")
+            command = ui.get_input("> ").strip().lower()
+            if command == "":
+                return name
+            elif command == "нет":
+                break
+            else:
+                ui.pause("\nтакой команды не существует.")
+
+def perk_choose(ui):
+    while True:
+        ui.display("\nвыбери свой перк. тебе на выбор доступно: арбитр, дрифтер, иммолятор, берсеркер, артиллерист")
+        perk = ui.get_input("> ").lower().strip()
+        if perk in ["арбитр", "дрифтер", "иммолятор", "берсеркер", "артиллерист"]:
+            while True:
+                ui.display(f"\nтвой перк - {perk}. нажми Enter чтобы продолжить или нет, чтобы сменить его")
+                command = ui.get_input("> ").lower().strip()
+                if command in ["нет", "сменить", "изменить"]:
+                    break
+                elif command == "":
+                    return perk
+                else:
+                    ui.pause("\nтакой команды не существует")
+        else:
+            ui.pause("\nтакого перка не существует")
+
+def game_over(ui):
+    ui.display("ты проиграл, увы")
+
+def not_ready(ui, *_):
+    ui.display("\nувы, данная функция пока что не готова")
+    ui.pause()
+
+def nothing(ui, *_):
+    ui.display("\nувы, ты ничего не нашел")
+    ui.pause()
