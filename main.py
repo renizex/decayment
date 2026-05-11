4e59662ebaccd10768b9cc62e99a978c4da44c12
renizex <xr4tygd@gmail.com>
2026-05-11 23:25:04 +0300

    menu() moved to ui.py

--- main.py
+++ main.py
@@ -1,31 +1,11 @@
 import random
 from data import classes
 from entities import Player
-from ui import UI, statistic
+from ui import UI, menu
 from utils import time_left
 from actions import name_create, perk_choose
 from items import shop, inventory_main
 from event import loot
-
-def menu(time, player, ui):
-    while time > 0:
-        display_time = time_left(time)
-        ui.display(f"\nу тебя осталось времени: {display_time}")
-        ui.display("тебе доступно пять опций: просмотр статистики, взаимодействие с инвентарем, поход в магазин, поход на вылазку, выход")
-        ui.display("для выбора пиши статистика, инвентарь, магазин, вылазка и выход соответственно")
-        choice = ui.get_input("> ").lower().strip()
-        if choice in ["1", "статистика", "стат"]:
-            statistic(player, ui)
-        elif choice in ["2", "инвентарь", "инвент", "инв"]:
-            inventory_main(ui, player)
-        elif choice in ["3", "магазин", "магаз", "маг"]:
-            shop(player, ui)
-        elif choice in ["4", "вылазка", "вылаз", "выл"]:
-            time = loot(player, ui, time)
-        elif choice in ["5", "0", "выход", "выйти"]:
-            break
-        else:
-            ui.pause("\nтакой команды не существует")
 
 def main():
     ui = UI()
@@ -34,7 +14,7 @@ def main():
     perk = perk_choose(ui)
     attr = classes[perk]["attributes"]
     player = Player(name, perk, attr)
-    menu(time, player, ui)
+    menu(time, player, ui, time_left, inventory_main, shop, loot)
 
 # начало кода
 print("я добавил взаимодействие через цифры и сокращения. поиграйся если интересно и лень писать полные названия для взаимодействия.")
