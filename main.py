c5323183307d66959e349e94e7ab1e38390d25fd
renizex <xr4tygd@gmail.com>
2026-05-16 22:41:29 +0300

    init player.enemies, drop and location

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
-from data import classes
+from data import classes, locations_events, drop, enemies
 from entities import Player
 from ui import UI, menu
-from utils import time_left
+from utils import time_left, spend_time
 from actions import name_create, perk_choose
 from items import shop, inventory_main
 from event import loot
@@ -14,7 +14,10 @@ def main():
     perk = perk_choose(ui)
     attr = classes[perk]["attributes"]
     player = Player(name, perk, attr)
-    menu(time, player, ui, time_left, inventory_main, shop, loot)
+    player.enemies = enemies
+    player.drop = drop
+    player.locations = locations_events
+    menu(time, player, ui, time_left, inventory_main, spend_time, shop, loot)
 
 # начало кода
 print("я добавил взаимодействие через цифры и сокращения. поиграйся если интересно и лень писать полные названия для взаимодействия.")
